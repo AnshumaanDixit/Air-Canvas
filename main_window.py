f6b3884af6e62ddeb7c05d016eeda9c1f2f6360a
AnshumaanDixit <2k25csai2511187@gmail.com>
2026-04-16 21:41:30 +0530

    new additions!

---
+++ main_window.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+import subprocess
+global air_controller
+def run_air_controller():
+    air_controller = subprocess.Popen(["python","Code/aircontroller.py"])
+    return air_controller
+main = tk.Tk()
+main.title("Air Controller")
+main.geometry("800x400")
+tk.Button(main,text="Air Controller",command=run_air_controller,state=tk.NORMAL,width=10,height=5).pack()
+main.mainloop()
