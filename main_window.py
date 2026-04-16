02c69e283391a260fce65d92785927a8392ddbd4
Pratham Dixit <pratham@Prathams-MacBook-Air.local>
2026-04-17 00:47:32 +0530

    Edited the main iterface

--- main_window.py
+++ main_window.py
@@ -2,10 +2,11 @@ import tkinter as tk
 import subprocess
 global air_controller
 def run_air_controller():
+    global button
     air_controller = subprocess.Popen(["python","aircontroller.py"])
     return air_controller
 main = tk.Tk()
 main.title("Air Controller")
 main.geometry("800x400")
-tk.Button(main,text="Air Controller",command=run_air_controller,state=tk.NORMAL,width=10,height=5).pack()
+button = tk.Button(main,text="Air Controller",command=run_air_controller,state=tk.NORMAL,width=25,height=5,relief=tk.RAISED,bd=10,cursor="hand2").grid(row=0,column=0)
 main.mainloop()
